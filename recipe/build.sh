8174cd453f7413dc74788f65ee8427ca81f3c996
Megan Yancy <myancy@anaconda.com>
2020-09-17 15:23:00 -0500

    in-progress build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,10 @@ cmake -LAH                            \
     -DCMAKE_BUILD_TYPE=Release        \
     -DCMAKE_INSTALL_PREFIX=$PREFIX    \
     -DCMAKE_INSTALL_LIBDIR=lib        \
+    -DCMAKE_CXX_FLAGS=$CXXFLAGS       \
+    -DCMAKE_C_FLAGS=$CFLAGS           \
     ..
 
-make -j${CPU_COUNT}
+
+make -j${CPU_COUNT} VERBOSE=1
 make install
